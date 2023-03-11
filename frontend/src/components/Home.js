a32372a539e07a8ad209c1fb4fc239636a597ab5
Samuel Padmadan <77727986+SamOp22@users.noreply.github.com>
2023-03-12 00:54:57 +0530

    Homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,12 +2,44 @@ import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { Hnavbar } from "./Hnavbar";
+import "../css/Home.css"
+
 
 export default function Home() {
   return (
     <div>
+
+
+
       <Hnavbar />
       <Navbar />
+      <div className="images">
+
+        <Link to="/donate">
+          <div className="image">
+            <img src="./donate.png" alt="" />
+
+            <h1>DONATE</h1>
+          </div>
+        </Link>
+
+
+        <Link to="/request">
+          <div className="image">
+            <h2>REQUEST</h2>
+            <img src="./receive.png" alt="" />
+          </div>
+        </Link>
+
+
+        <Link to="/volunteer">
+          <div className="image">
+            <img src="./vollunteer.png" alt="" />
+            <h1>VOLUNTEER</h1>
+          </div>
+        </Link>
+
+      </div>
     </div>
   );
 }
