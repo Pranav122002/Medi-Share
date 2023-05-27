4d17909ba9bcfe99693d595c099e87bb437a8176
Samuel Padmadan <77727986+SamOp22@users.noreply.github.com>
2023-05-27 18:17:35 +0530

    About us images remaining

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import { Hnavbar } from "./Hnavbar";
+import  Navbar from "./Navbar";
+import "../css/Aboutus.css";
+
 export const AboutUs = () => {
   return (
-    <>
+    <div className='abou'>
      <Hnavbar />
-     <div>AboutUs</div>
-    </>
+     
+     <div className='aboutus'>
+        <h1>About us</h1>
+        <p>We are a passionate and dedicated team that has come together to build an extraordinary project. With a shared vision and diverse expertise, we have collaborated tirelessly to create something truly remarkable. Our team comprises individuals from various backgrounds, including software development, ui ux design, and project hosting , ML enthusiast each bringing unique skills and perspectives to the table.
+
+</p>
+        <div className='Aboutimage'>
+            <div className='imageee'>
+                <img src="" alt="img" srcset="" />
+                <h2>Pranav Patil</h2>
+            </div>
+            <div className='imageee'>
+                <img src="sam.jpeg" alt="" srcset="" />
+                <h2>Samuel Padmadan</h2>
+            </div>
+            <div className='imageee'>
+                <img src="" alt="img" srcset="" />
+                <h2>Paul Crescent Palanisamy</h2>
+            </div>
+            <div className='imageee'>
+                <img src="" alt="img" srcset="" />
+                <h2>Aditya Patil</h2>
+            </div>
+        </div>
+     </div>
+    </div>
     
   
   )
