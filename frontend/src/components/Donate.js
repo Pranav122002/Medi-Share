07deb516bf8bd5bce402b6bc989b44ccbcc637df
Pauloper1 <paulcrescent.p@gmail.com>
2023-06-21 18:44:05 +0530

    Donate form Validation added

--- frontend/src/components/Donate.js
+++ frontend/src/components/Donate.js
@@ -8,14 +8,14 @@ import Medicines from "./Medicines"
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { API_BASE_URL } from "../config";
-import { REACT_APP_MAP_API_KEY } from '../keys'
 import geocode from './geocodeFun'
+import * as Yup from 'yup';
 import Modal from 'react-modal'
 
 export default function Donate() {
   const [medicineForms, setMedicineForms] = useState([])
   const [medicine_name, setMedicineName] = useState("");
-  const [quantity, setQuantity] = useState("");
+  const [quantity, setQuantity] = useState(null);
   const [expiry_date, setExpiryDate] = useState(new Date);
   const [location, setLocation] = useState("");
   const [count, setCount] = useState(1);
@@ -24,7 +24,19 @@ export default function Donate() {
   const [coordinates, setCoordinates] = useState(null)
   const [medicineList, setMedicineList] = useState([])
 
-  const handleAddForm = () => {
+
+
+  const handleAddForm = async () => {
+    try {
+      const validation = await validationSchema.validate(
+        { medicine_name: medicine_name,
+          expiry_date: expiry_date,
+          quantity: quantity
+         },
+        { abortEarly: false }
+      );
+      console.log("validation", validation);
+
     if (medicine_name !== "" && expiry_date !== "" && quantity !== "" && location !== "") {
       setMedicineForms((previousForms) => [
         ...previousForms,
@@ -44,6 +56,11 @@ export default function Donate() {
       console.log("formMover " + formMover)
       console.log("count " + count)
     }
+  } catch (error) {
+    error.inner.forEach((validationError) => {
+      notifyA(validationError.message)
+    });
+  }
   }
 
   const handLeftForm = () => {
@@ -106,7 +123,16 @@ export default function Donate() {
   const notifyA = (msg) => toast.error(msg);
   const notifyB = (msg) => toast.success(msg);
 
-  const postOrderData = () => {
+  const postOrderData = async() => {
+    try {
+      const validation = await validationSchema.validate(
+        { medicine_name: medicine_name,
+          expiry_date: expiry_date,
+          quantity: quantity
+         },
+        { abortEarly: false }
+      );
+      console.log("validation", validation);
 
     geocode(location)
       .then(coordinates => {
@@ -157,111 +183,110 @@ export default function Donate() {
             console.log(data);
           });
       });
+    } catch (error) {
+      error.inner.forEach((validationError) => {
+        notifyA(validationError.message)
+      });
+    }
   };
 
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
-
-  // const fetchMedicines = (query) => {
-  //   setSearch(query);
-  //   fetch(`${API_BASE_URL}/search-medicines`, {
-  //     method: "post",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       query,
-  //     }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((results) => {
-  //       setSearchResult(results.medicine);
-  //     });
-  // };
-
-
+//Validate form
+  const validationSchema = Yup.object().shape({
+    medicine_name: Yup.string()
+      .oneOf(medicineList, 'Invalid medicine')
+      .required('Medicine name is required'),
+    expiry_date: Yup.date()
+      .min(new Date(), 'Expiry date must be in the future')
+      .required('Expiry date is required'),
+    quantity: Yup.number()
+      .min(1,'Quantity can not be less than one')
+      .required('Quantity is required')
+  });
 
   return (
     <div className="donateeapp">
       <div className="bodyy">
         <div className="donatecont">
-        <div className="donate_instru">
-          <div className="donate_content">
-            <h1>Some Important Instructions for Donating</h1>
-            {/* <img data-aos="fade-down-right" src="./medicine.png" alt="" /> */}
-            <div className="points">
-              <p>
-                1.The medicine to be donated should be valid and not expired or
-                fabricated.
-              </p>
-              <p>2.The medicine name , expiry date should be visible.</p>
+          <div className="donate_instru">
+            <div className="donate_content">
+              <h1>Some Important Instructions for Donating</h1>
+              {/* <img data-aos="fade-down-right" src="./medicine.png" alt="" /> */}
+              <div className="points">
+                <p>
+                  1.The medicine to be donated should be valid and not expired or
+                  fabricated.
+                </p>
+                <p>2.The medicine name , expiry date should be visible.</p>
+              </div>
             </div>
+            <img src="./donmed.jpg" data-aos="fade-right" alt="" srcSet="" />
           </div>
-          <img src="./donmed.jpg" data-aos="fade-right" alt="" srcSet="" />
-        </div>
 
-        <div className="donate">
-          <div data-aos="fade-right" className="donateForm">
-            <div className="logo">
-              <h1>Donate Medicine</h1>
-            </div>
-            <div>
-              {/* <h4>{(formMover%count)+1}</h4> */}
-              {/* list name and datalist id must be same */}
-              <input 
-              list="medicine"
-              id="medicine_name"
-              name="medicine_name"
-              value={medicine_name}
-              placeholder="Medicine Name"
-              onChange={(e) => {
-                setMedicineName(e.target.value);
-              }}
-              />
-              <datalist
-              id="medicine"
-              >
-              {medicineList.map((item=>
-              <option>{item}</option>
-                ))}
-              </datalist>
-            </div>
-            <div>
-              <input
-                type="date"
-                name="expiry_date"
-                id="expiry_date"
-                placeholder="Expiry Date"
-                value={expiry_date}
-                onChange={(e) => {
-                  setExpiryDate(e.target.value);
-                }}
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                name="quantity"
-                id="quantity"
-                placeholder="Quantity"
-                value={quantity}
-                onChange={(e) => {
-                  setQuantity(e.target.value);
-                }}
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                name="location"
-                id="location"
-                placeholder="Location"
-                value={location}
-                onChange={(e) => {
-                  setLocation(e.target.value);
-                }}
-              />
-              {/* <div>
+          <div className="donate">
+            <div data-aos="fade-right" className="donateForm">
+              <div className="logo">
+                <h1>Donate Medicine</h1>
+              </div>
+              <div>
+                {/* <h4>{(formMover%count)+1}</h4> */}
+                {/* list name and datalist id must be same */}
+
+                <input
+                  list="medicine"
+                  id="medicine_name"
+                  name="medicine_name"
+                  value={medicine_name}
+                  placeholder="Medicine Name"
+                  onChange={(e) => {
+                    setMedicineName(e.target.value);
+                  }}
+                />
+                <datalist
+                  id="medicine"
+                >
+                  {medicineList.map((item =>
+                    <option>{item}</option>
+                  ))}
+                </datalist>
+              </div>
+              <div>
+                <input
+                  type="date"
+                  name="expiry_date"
+                  id="expiry_date"
+                  placeholder="Expiry Date"
+                  value={expiry_date}
+                  onChange={(e) => {
+                    setExpiryDate(e.target.value);
+                  }}
+                />
+              </div>
+              <div>
+                <input
+                  type="number"
+                  name="quantity"
+                  id="quantity"
+                  placeholder="Quantity"
+                  value={quantity}
+                  onChange={(e) => {
+                    setQuantity(e.target.value);
+                  }}
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="location"
+                  id="location"
+                  placeholder="Location"
+                  value={location}
+                  onChange={(e) => {
+                    setLocation(e.target.value);
+                  }}
+                />
+                {/* <div>
                 {selectedImage ? (
                   <Modal
                     className="Model__Container"
@@ -278,52 +303,52 @@ export default function Donate() {
                   <button onClick={handleTakePhoto}>Take Photo</button>
                 </div>
               </div> */}
+              </div>
+              <div>
+                <button onClick={() => { handLeftForm() }}>Left</button>
+                <button onClick={() => { handleAddForm() }}>Add</button>
+                <button onClick={() => { handleRightForm() }}>Right</button>
+              </div>
+              <button
+                className="button-53"
+                onClick={() => {
+                  postOrderData();
+                }}
+                value="Donate"
+                type="submit"
+                role="button"
+              >
+                Donate
+              </button>
             </div>
-            <div>
-              <button onClick={() => { handLeftForm() }}>Left</button>
-              <button onClick={() => { handleAddForm() }}>Add</button>
-              <button onClick={() => { handleRightForm() }}>Right</button>
+            <div className={`suggestions ${sug && 'active'}`}  >
+
+              <ul>
+                <li style={{ color: "black" }}>
+                  <h2>Suggestions</h2>
+                </li>
+                {searchResult.map((item) => {
+                  return (
+
+                    <li className="link">
+                      <h3 style={{ color: "black" }}>
+
+                        {item.medicine_name + ": " + "-"}
+                      </h3>
+
+                      <h3 className="p2" style={{ color: "black" }}>{item.disease}</h3>
+
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
-            <button
-              className="button-53"
-              onClick={() => {
-                postOrderData();
-              }}
-              value="Donate"
-              type="submit"
-              role="button"
-            >
-              Donate
-            </button>
+
+            <div data-aos="zoom-in" className="donateback">
+              <h1>"No one has ever become poor from giving"</h1>
+              <p>-Anne Frank</p>
+            </div>
           </div>
-          <div className={`suggestions ${sug && 'active'}`}  >
-
-            <ul>
-              <li style={{ color: "black" }}>
-                <h2>Suggestions</h2>
-              </li>
-              {searchResult.map((item) => {
-                return (
-
-                  <li className="link">
-                    <h3 style={{ color: "black" }}>
-
-                      {item.medicine_name + ": " + "-"}
-                    </h3>
-
-                    <h3 className="p2" style={{ color: "black" }}>{item.disease}</h3>
-
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-
-          <div data-aos="zoom-in" className="donateback">
-            <h1>"No one has ever become poor from giving"</h1>
-            <p>-Anne Frank</p>
-          </div>
-        </div>
         </div>
       </div>
     </div>
