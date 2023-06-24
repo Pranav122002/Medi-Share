b9d1b42a97fb80ac3bc5822f7fa44bce5adbc1d6
Samuel Padmadan <77727986+SamOp22@users.noreply.github.com>
2023-06-25 01:01:40 +0530

    Heart disease responsive

--- frontend/src/components/heartDisease.js
+++ frontend/src/components/heartDisease.js
@@ -6,6 +6,8 @@ import { Hnavbar } from "./Hnavbar";
 import { fetchUser, handleSaveReport, fetchReport } from "../Functions/reportFunctions"
 import ReportModal from "../Functions/ReportModal";
 import Modal from 'react-modal'
+import "../css/Heartdis.css"
+
 
 function HeartDisease() {
 
@@ -87,181 +89,207 @@ function HeartDisease() {
 
   return (
     <>
-      <div className="heartdis">
-        <h1>Heart Disease Prediction</h1>
-        <form onSubmit={handleSubmit}>
-          {/* Add input fields for each feature */}
+      <div className="heartdismain">
+        <div className="heartdis">
+          <h2>Heart Disease Prediction</h2>
+          <div className="heartrepo">
+            <button id='traarports' onClick={handleShowReport}>Previous Reports</button>
+            <Modal
+              className="Modal__container"
+              isOpen={reportModal}
+              onRequestClose={() => { setReportModal(false) }}
+              style={{
+                overlay: {
+                  zIndex: 9999
+                },
+                content: {
+                  zIndex: 9999
+                }
+              }}
+            >
+              {reports.map((report, index) =>
+                <div key={index}>
+                  <p>Report: {report._id.toString().slice(-4)}</p>
+                  <p>Result: {report.result}</p>
+                  <p>Date: {report.report_creation_date}</p>
+                  <button onClick={() => setMoreInfoModal(true)}>Info</button>
+                  <button onClick={() => handleSet(report)}>Set</button>
+                  <Modal
+                    className="Modal__container"
+                    isOpen={moreInfoModal}
+                    onRequestClose={() => { setMoreInfoModal(false) }}
+                    style={{
+                      overlay: {
+                        zIndex: 9999
+                      },
+                      content: {
+                        zIndex: 9999
+                      }
+                    }}
+                  >
+                    <h2>Heart Disease Report</h2>
+                    <p>Age: {report.age}</p>
+                    <p>Sex: {report.sex}</p>
+                    <p>Chest Pain Type: {report.cp}</p>
+                    <p>Resting Blood Pressure: {report.trestbps}</p>
+                    <p>Serum Cholesterol: {report.chol}</p>
+                    <p>Fasting Blood Sugar: {report.fbs}</p>
+                    <p>Resting Electrocardiographic Results: {report.restecg}</p>
+                    <p>Maximum Heart Rate Achieved: {report.thalach}</p>
+                    <p>Exercise Induced Angina: {report.exang}</p>
+                    <p>ST Depression Induced by Exercise Relative to Rest: {report.oldpeak}</p>
+                    <button onClick={() => { setMoreInfoModal(false) }}>Close</button>
+                  </Modal>
+                </div>
+              )}
+              <button onClick={() => { setReportModal(false) }}>Close</button>
 
-          <div className="flexxx">
-            <label>Age:</label>
-            <input
-              type="number"
-              name="age"
-              value={formData.age}
-              onChange={handleInputChange}
-            />
+            </Modal>
           </div>
-          <div className="flexxx">
-            <label>Sex :</label>
-            <select type="number" onChange={handleInputChange}  value={formData.sex} name="sex" id="">
+          <form onSubmit={handleSubmit}>
+            {/* Add input fields for each feature */}
+            <div className="heartdisflex">
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>Age:</label>
+                  <input
+                    type="number"
+                    name="age"
+                    value={formData.age}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>Sex :</label>
+                  <select type="number" onChange={handleInputChange} value={formData.sex} name="sex" id="">
 
-              <option value="1">male</option>
-              <option value="0">female</option>
-            </select>
-            {/* <input
+                    <option value="1">male</option>
+                    <option value="0">female</option>
+                  </select>
+                  {/* <input
               type="number"
               name="sex"
               value={formData.sex}
               onChange={handleInputChange}
             /> */}
-          </div>
-          <div className="flexxx">
-            <label>
-              Cp (chest pain type(0 = no pain; 1 = less pain; 2 = medium pain; 3
-              = sevier pain;)):
-            </label>
-            <input
-              type="number"
-              name="cp"
-              value={formData.cp}
-              onChange={handleInputChange}
-            />
-          </div>
+                </div>
+              </div>
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>
+                    Chest pain
+                  </label>
 
-          <div className="flexxx">
-            <label>Resting Blood Pressure(in mm):</label>
-            <input
-              type="number"
-              name="trestbps"
-              value={formData.trestbps}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="flexxx">
-            <label>serum cholestoral (in mg/dl):</label>
-            <input
-              type="number"
-              name="chol"
-              value={formData.chol}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="flexxx">
-            <label>
-              fasting blood sugar 120 mg/dl or greater (1 = true; 0 = false):
-            </label>
-            <input
-              type="number"
-              name="fbs"
-              value={formData.fbs}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="flexxx">
-            <label>Resting Electrocardiographic:</label>
-            <input
-              type="number"
-              name="restecg"
-              value={formData.restecg}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="flexxx">
-            <label>Maximum Heart Rate:</label>
-            <input
-              type="number"
-              name="thalach"
-              value={formData.thalach}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="flexxx">
-            <label>Exercise Induced Angina (1 = yes; 0 = no):</label>
-            <input
-              type="number"
-              name="exang"
-              value={formData.exang}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="flexxx">
-            <label>
-              ST depression induced by exercise relative to rest(Range 0 - 6.2):
-            </label>
-            <input
-              type="number"
-              name="oldpeak"
-              value={formData.oldpeak}
-              onChange={handleInputChange}
-            />
-          </div>
+                  <select type="number" value={formData.cp} onChange={handleInputChange} name="cp" id="">
+                    <option value="0">No pain</option>
+                    <option value="1">Less pain</option>
+                    <option value="2">Medium pain</option>
+                    <option value="3">Sevier pain</option>
+                  </select>
+                </div>
+              </div>
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>Resting Blood Pressure(in mm):</label>
+                  <input
+                    type="number"
+                    name="trestbps"
+                    value={formData.trestbps}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>serum cholestoral (in mg/dl):</label>
+                  <input
+                    type="number"
+                    name="chol"
+                    value={formData.chol}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>
+                    fasting blood sugar
+                  </label>
 
-          {/* Add input fields for other features */}
-          <button className="button-53" value="submit" type="submit">
-            Predict
-          </button>
-        </form>
-        {prediction !== null && (
-          <p>
-            Prediction:{" "}
-            {prediction === 1 ? "Heart Disease" : "No Heart Disease"}
-            <button onClick={handleSave}>Save report</button>
-          </p>
-        )}
-        <button onClick={handleShowReport}>Show Reports</button>
-        <Modal
-          className="Modal__container"
-          isOpen={reportModal}
-          onRequestClose={() => { setReportModal(false) }}
-          style={{
-            overlay: {
-              zIndex: 9999
-            },
-            content: {
-              zIndex: 9999
-            }
-          }}
-        >
-          {reports.map((report, index) =>
-            <div key={index}>
-              <p>Report: {report._id.toString().slice(-4)}</p>
-              <p>Result: {report.result}</p>
-              <p>Date: {report.report_creation_date}</p>
-              <button onClick={() => setMoreInfoModal(true)}>Info</button>
-              <button onClick={() => handleSet(report)}>Set</button>
-              <Modal
-                className="Modal__container"
-                isOpen={moreInfoModal}
-                onRequestClose={() => { setMoreInfoModal(false) }}
-                style={{
-                  overlay: {
-                    zIndex: 9999
-                  },
-                  content: {
-                    zIndex: 9999
-                  }
-                }}
-              >
-                <h2>Heart Disease Report</h2>
-                <p>Age: {report.age}</p>
-                <p>Sex: {report.sex}</p>
-                <p>Chest Pain Type: {report.cp}</p>
-                <p>Resting Blood Pressure: {report.trestbps}</p>
-                <p>Serum Cholesterol: {report.chol}</p>
-                <p>Fasting Blood Sugar: {report.fbs}</p>
-                <p>Resting Electrocardiographic Results: {report.restecg}</p>
-                <p>Maximum Heart Rate Achieved: {report.thalach}</p>
-                <p>Exercise Induced Angina: {report.exang}</p>
-                <p>ST Depression Induced by Exercise Relative to Rest: {report.oldpeak}</p>
-                <button onClick={() => { setMoreInfoModal(false) }}>Close</button>
-              </Modal>
+                  <select value={formData.fbs} onChange={handleInputChange} type="number" name="fbs" id="">
+                    <option value="0">less than 120 mg/dl</option>
+                    <option value="1">Greater than 120 mg/dl</option>
+
+                  </select>
+                </div>
+              </div>
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>Resting Electrocardiographic:</label>
+                  <input
+                    type="number"
+                    name="restecg"
+                    value={formData.restecg}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>Maximum Heart Rate:</label>
+                  <input
+                    type="number"
+                    name="thalach"
+                    value={formData.thalach}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>Exercise Induced Angina :</label>
+                  <select onChange={handleInputChange} value={formData.exang} type="number" name="exang" id="">
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                  </select>
+                </div>
+              </div>
+              <div className="flexxx">
+                <div className="heartflexbox">
+                  <label>
+                    ST depression induced by exercise relative to rest (Range 0 - 6.2):
+                  </label>
+                  <input
+                    type="number"
+                    name="oldpeak"
+                    value={formData.oldpeak}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
             </div>
-          )}
-          <button onClick={() => { setReportModal(false) }}>Close</button>
+            {/* Add input fields for other features */}
+            <button className="button-53" value="submit" type="submit">
+              Predict
+            </button>
+          </form>
+          {prediction !== null && (
+            <p id="hesatdiuscno">
+              Prediction:{" "}
+              {prediction === 1 ? "Heart Disease detected" : "Heart Disease not found"}
 
-        </Modal>
+            </p>
+
+          )}
+            {prediction &&  <button  id='kidimgp' onClick={handleSave}>Save report</button> }
+         
+
+        </div>
       </div>
     </>
+
   );
 }
 
